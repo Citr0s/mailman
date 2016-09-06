73549d769723b99fd4354ae56ceae08d4132390b
Milosz Dura <milosz.md@wp.pl>
2016-09-06 12:34:27 +0100

    now making requests through backend

--- renderer.js
+++ renderer.js
@@ -1,10 +1,4 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-var request = require('request');
 const ipc = require('electron').ipcRenderer;
-
 var mainRequestType = document.getElementsByClassName('main-request-type')[0];
 var mainRequestLink = document.getElementsByClassName('main-request-link')[0];
 var mainButton = document.getElementsByClassName('main-submit-button')[0];
@@ -15,7 +9,6 @@ var requestEditorHidden = document.getElementsByClassName('request-editor-hidden
 var previousRequestList = document.getElementsByClassName('previous-request-list')[0];
 var previousRequestsDiv = document.getElementsByClassName('previous-request');
 var statusDiv = document.getElementsByClassName('response-status')[0];
-
 var previousRequests = [];
 
 function triggerEvent(eventType, element){
@@ -58,86 +51,29 @@ mainButton.addEventListener('click', function(e){
       }
     }
 
-    makeRequest(mainRequestType.value, mainRequestLink.value, headers, requestEditorHidden.value);
+    var requestDetails = {
+      requestType: mainRequestType.value,
+      requestLink: mainRequestLink.value,
+      requestHeader: headers,
+      requestBody: requestEditorHidden.value,
+    };
 
-    var previousRequestsHtml = '';
-
-    for(var k = 0; k < previousRequests.length; k++){
-      previousRequestsHtml += '<div class="previous-request" data-type="' + previousRequests[k].requestType + '" data-link="' + previousRequests[k].requestLink + '" data-header=\'' + JSON.stringify(previousRequests[k].requestHeader) + '\' data-body="' + previousRequests[k].requestBody + '"><small class="' + previousRequests[k].requestType + '">' + previousRequests[k].requestType + '</small>' + previousRequests[k].requestLink + '</div>';
-    }
-
-    // previousRequestList.innerHTML = previousRequestsHtml;
-
-    for(var l = 0; l < previousRequestsDiv.length; l++){
-      // previousRequestsDiv[l].addEventListener('click', function(e){
-      //   mainRequestType.value = this.getAttribute('data-type');
-      //   mainRequestLink.value = this.getAttribute('data-link');
-      //   requestEditorHidden.innerHTML = this.getAttribute('data-body');
-      //   triggerEvent('change', requestEditorHidden);
-      //
-      //   var storedHeaders = JSON.parse(this.getAttribute('data-header'));
-      //
-      //   var size = -1;
-      //   for(key in storedHeaders){
-      //     size++;
-      //   }
-      //
-      //   var count = 1;
-      //   for(key in storedHeaders){
-      //     if(typeof headerNames[count] === 'undefined'){
-      //       var headerRowTemplate = document.getElementsByClassName('header-row-template')[0];
-      //       var mainForm = document.getElementsByClassName('main-submit-form')[0];
-      //       var headerAddButtons = document.getElementsByClassName('header-add-button');
-      //
-      //       newHeaderRow = document.createElement('div');
-      //       newHeaderRow.setAttribute('class', 'row');
-      //       newHeaderRow.innerHTML = headerRowTemplate.innerHTML;
-      //       mainForm.appendChild(newHeaderRow);
-      //       updateHeaderButtonListeners(headerAddButtons, newHeaderRow, headerRowTemplate, mainForm);
-      //     }
-      //     headerNames[count].value = key;
-      //     headerValues[count].value = storedHeaders[key];
-      //     count++;
-      //   }
-      //
-      //   for(var m = count; m <= headerNames.length; m++){
-      //     var element = headerNames[m].parentElement.parentElement.parentElement;
-      //     element.parentElement.removeChild(element);
-      //   }
-      // });
-    }
+    ipc.send('makeRequest', requestDetails);
+    mainButton.setAttribute('disabled', '');
+    ipc.on('makeResponse', function(e, data) {
+      statusDiv.innerHTML = "<span class='_599'>No response.</span>";
+      mainBodyHidden.value = '';
+      if(typeof data.response !== 'undefined'){
+        statusDiv.innerHTML = "<span class='_" + data.response.statusCode + "'>" + data.response.statusCode + " " + data.response.statusMessage + "</span>";
+        mainBodyHidden.value = JSON.stringify(JSON.parse(data.body), undefined, 2);
+      }
+      mainButton.removeAttribute('disabled');
+      triggerEvent('change', mainBodyHidden);
+    });
   }, 1);
 });
 
-function makeRequest(requestType, requestLink, requestHeader, requestBody){
-  var requestDetails = {
-    'requestType':requestType,
-    'requestLink':requestLink,
-    'requestHeader':requestHeader,
-    'requestBody':requestBody,
-  };
-  previousRequests.push(requestDetails);
-
-  request({
-    method: requestType,
-    preambleCRLF: true,
-    postambleCRLF: true,
-    uri: requestLink,
-    headers: requestHeader,
-    body: requestBody,
-  },
-  function (error, response, body) {
-    statusDiv.innerHTML = "<span class='_599'>No response.</span>";
-    mainBodyHidden.value = '';
-    if(typeof response !== 'undefined'){
-      statusDiv.innerHTML = "<span class='_" + response.statusCode + "'>" + response.statusCode + " " + response.statusMessage + "</span>";
-      mainBodyHidden.value = JSON.stringify(JSON.parse(body), undefined, 2);
-    }
-    triggerEvent('change', mainBodyHidden);
-  });
-}
-
-var vue = new Vue({
+new Vue({
   el: '#app',
   data: {
     requestType: 'get',
@@ -147,28 +83,28 @@ var vue = new Vue({
     clicked: [false]
   },
   methods: {
-    addPreviousRequest: () => {
+    addPreviousRequest: function() {
       var requestHeaders = [];
-      for(var i = 0; i < vue.requestHeaders.length; i++){
-        requestHeaders.push({name: vue.requestHeaders[i].name, value: vue.requestHeaders[i].value});
+      for(var i = 0; i < this.requestHeaders.length; i++){
+        requestHeaders.push({name: this.requestHeaders[i].name, value: this.requestHeaders[i].value});
       }
       var previousRequest = {
-        requestType: vue.requestType,
-        requestLink: vue.requestLink,
+        requestType: this.requestType,
+        requestLink: this.requestLink,
         requestHeaders: requestHeaders,
       };
-      vue.clicked[vue.clicked.length] = false;
-      vue.previousRequests.push(previousRequest);
+      this.clicked[this.clicked.length] = false;
+      this.previousRequests.push(previousRequest);
     },
-    populateRequestForm: (index) => {
-      var selectedRequest = vue.previousRequests[index];
-      vue.requestType = selectedRequest.requestType;
-      vue.requestLink = selectedRequest.requestLink;
-      vue.requestHeaders = selectedRequest.requestHeaders;
+    populateRequestForm: function(index) {
+      var selectedRequest = this.previousRequests[index];
+      this.requestType = selectedRequest.requestType;
+      this.requestLink = selectedRequest.requestLink;
+      this.requestHeaders = selectedRequest.requestHeaders;
     },
-    addHeaderRow: (index) => {
-      vue.clicked[index] = true;
-      vue.requestHeaders.push({});
+    addHeaderRow: function(index) {
+      this.clicked[index] = true;
+      this.requestHeaders.push({});
     },
   },
 })
